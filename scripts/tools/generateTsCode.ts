b5d0a107f45cf01422332a919aa730935d83ec01
Himenon <k.himeno314@gmail.com>
2021-04-07 16:23:25 +0900

    chore(deps): update library

--- scripts/tools/generateTsCode.ts
+++ scripts/tools/generateTsCode.ts
@@ -1,19 +1,26 @@
 import * as fs from "fs";
 import * as path from "path";
 import * as logger from "./logger";
-import { generateTypeScriptCode } from "@himenon/openapi-typescript-code-generator";
 
-export const generateTsCode = (
-  entryPoint: string,
-  outputFileName: string
-): void => {
-  const tsSourceCode = generateTypeScriptCode({
-    entryPoint,
-    enableValidate: false,
-  });
+import { CodeGenerator } from "@himenon/openapi-typescript-code-generator";
+import * as Templates from "@himenon/openapi-typescript-code-generator/templates";
+import type * as Types from "@himenon/openapi-typescript-code-generator/types";
+
+export const generateTsCode = (entryPoint: string, outputFileName: string): void => {
+  const codeGenerator = new CodeGenerator(entryPoint);
+
+  const apiClientGeneratorTemplate: Types.CodeGenerator.CustomGenerator<Templates.ApiClient.Option> = {
+    generator: Templates.ApiClient.generator,
+    option: {},
+  };
+
+  const code = codeGenerator.generateTypeDefinition([
+    codeGenerator.getAdditionalTypeDefinitionCustomCodeGenerator(),
+    apiClientGeneratorTemplate,
+  ]);
 
   fs.mkdirSync(path.dirname(outputFileName), { recursive: true });
-  fs.writeFileSync(outputFileName, tsSourceCode, {
+  fs.writeFileSync(outputFileName, code, {
     encoding: "utf-8",
   });
 
